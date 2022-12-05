f9dbc3debe9838fb2c21999c5d7451b4cb6bf16a
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2022-12-05 22:41:06 +0200

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -3,23 +3,23 @@
 
 using namespace std; 
 
-// stack is a data structure that follows the LIFO (last in first out) 
-
 
 class Stack{
   
   public:
-    int *arr; 
-    int size; 
-    int capacity; 
+    int *arr; // pointer to the array that will store the stack elements
+    int size;  // the number of elements in the stack
+    int capacity = 1e7; // the maximum capacity of the stack
   
+  // constructor
   Stack(){
     size = 0;
     capacity = 1;
     arr = new int[capacity]; 
   }
   
-  void push(int val){
+  // add an element to the top of the stack
+  void push(int val){ 
     if (size == capacity){ 
       int *temp = new int[2 * capacity]; 
       for (int i = 0; i < size; i++){
@@ -32,12 +32,14 @@ class Stack{
     arr[size++] = val;
   }
   
-  void pop(){
+  // remove the top element
+  void pop(){ 
     if (size == 0) return void(cout << "Stack is empty");
     size--;
   }
   
-  int top(){
+  // return the top element
+  int top(){ 
     if (size == 0) { 
       cout << "Stack is empty";
       return -1;
@@ -45,19 +47,23 @@ class Stack{
     return arr[size - 1];
   }
   
+  // return the size of the stack
   int get_size(){
-    return size;
+    return size; 
   }
   
+  // return the capacity of the stack
   int get_capacity(){
-    return capacity;
+    return capacity; 
   }
   
-  bool is_empty(){
+  // check if the stack is empty
+  bool is_empty(){ 
     return size == 0;
   }
   
-  void print(){
+  // print the stack
+  void print(){ 
     for (int i = 0; i < size; i++){
       cout << arr[i] << " ";
     }
@@ -65,28 +71,59 @@ class Stack{
   }
   
   ~Stack(){
-    delete arr;
+    delete arr; // free the memory
+  }
+
+  ll find_pos(int val){
+    for (int i = 0; i < size; i++){
+      if (arr[i] == val) return i; // return the position of the value
+    }
+    cout << "Value not found "; 
+    return -1; // if the value is not found
+  }
+
+  // check if the string is plandrom or not
+  bool is_plandrom(string s){ 
+    Stack st;
+    for (int i = 0; i < s.size(); i++){
+      st.push(s[i]); // push char as int .. The ASCII value of 'a' is 97, 'b' is 98, and so on
+    }
+    for (int i = 0; i < s.size(); i++){
+      if (char(st.top()) != s[i]){ // char() is used to convert int to char 
+        cout << "Not plandrom " << s << "\n";
+      }
+      st.pop();
+    }
+    cout << "Plandrom " << s << "\n";
+    return true;
+  }
+
+  // convert decimal to binary
+  string binary(ll n){
+    Stack st;
+    int temp = n;
+    while (n){
+      st.push(n % 2); // push() is used to add an element to the top of the stack
+      n /= 2; 
+    }
+    string s;
+    while (!st.is_empty()){
+      s += st.top() + '0'; // '0' is used to convert int to char
+      st.pop(); // pop() is used to remove the top element from the stack
+    }
+    cout << "Binary of " << temp << " is: ";
+    return s;
   }
   
 };
 
 
-
 int main(){
 
+  Stack st;
 
-   Stack st;
-   st.push(3);
-   st.push(4);
-   st.push(5);
-   st.push(6);
+  st.is_plandrom("abba");
+  cout <<  st.binary(7) << "\n";
 
-   st.print();
-
-   st.pop();
-
-   st.print();
-
-
-    return 0;
+  return 0;
 }
